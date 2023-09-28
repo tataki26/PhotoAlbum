147a65d5ac5d0c2e0785b3b23d881149098c202e
tataki26 <tataki26@gmail.com>
2023-09-28 17:33:01 +0900

    [domain] Photo 엔티티 추가
    - 연관 관계 매핑을 단방향 다대일로 설정
    - ** 추후 역방향으로 객체 탐색이 필요하다고 느끼면 양방향으로 매핑 관계 변경할 것 **
    
    - @ManyToOne의 default가 즉시 로딩이므로 지연 로딩 설정 추가
    - @JoinColumn으로 FK를 album_id로 지정

---
+++ server/photoalbum/src/main/java/com/tataki26/photoalbum/domain/Photo.java
@@ -0,0 +1,30 @@
+package com.tataki26.photoalbum.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Photo {
+    @Id @GeneratedValue
+    @Column(name = "photo_id", unique = true, nullable = false)
+    private Long id;
+    @Column(name = "photo_name", nullable = false)
+    private String name;
+    private String thumbUrl;
+    private String originalUrl;
+    private int fileSize;
+    @CreationTimestamp
+    private Date uploadedAt;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name="album_id")
+    private Album album;
+}
