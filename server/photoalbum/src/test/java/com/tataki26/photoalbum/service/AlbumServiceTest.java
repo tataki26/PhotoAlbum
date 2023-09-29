7b787c7e99e699ee773045a1dc07aa47e1e37cfe
tataki26 <tataki26@gmail.com>
2023-09-29 18:01:32 +0900

    [test] 앨범 정보 API Service 테스트 코드 구현
    - mock 앨범 생성(이름만 저장) 후 테스트
    - 저장한 이름과 저장된 이름 비교

---
+++ server/photoalbum/src/test/java/com/tataki26/photoalbum/service/AlbumServiceTest.java
@@ -0,0 +1,34 @@
+package com.tataki26.photoalbum.service;
+
+import com.tataki26.photoalbum.domain.Album;
+import com.tataki26.photoalbum.dto.AlbumDto;
+import com.tataki26.photoalbum.repository.AlbumRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+class AlbumServiceTest {
+    @Autowired
+    AlbumRepository albumRepository;
+    @Autowired
+    AlbumService albumService;
+
+    @Test
+    void retrieveAlbum() {
+        // insert album into persist context
+        Album album = new Album();
+        album.setName("test");
+        Album savedAlbum = albumRepository.save(album);
+
+        // get album from persist context
+        Album resultAlbum = albumService.retrieveAlbum(savedAlbum.getId());
+
+        assertEquals("test", resultAlbum.getName());
+    }
+
+}
