5db79f93c46499a50066be28f6155349f73db03d
tataki26 <tataki26@gmail.com>
2023-10-02 17:23:30 +0900

    [repository] 앨범 목록 API Repository 코드 구현
    - 검색 키워드(name)와 정렬 기준(keyword)으로 Album List를 반환하는 메서드 추가
    
    - findByNameContaining: 특정 키워드가 포함된 이름을 기준으로 찾기
    - OrderBy필드명ASC/DESC: 필드를 기준으로 오름차순/내림차순 정렬하여 반환

--- server/photoalbum/src/main/java/com/tataki26/photoalbum/repository/AlbumRepository.java
+++ server/photoalbum/src/main/java/com/tataki26/photoalbum/repository/AlbumRepository.java
@@ -4,9 +4,12 @@ import com.tataki26.photoalbum.domain.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
     Optional<Album> findByName(String name);
+    List<Album> findByNameContainingOrderByNameAsc(String name);
+    List<Album> findByNameContainingOrderByCreatedAtDesc(String keyword);
 }
