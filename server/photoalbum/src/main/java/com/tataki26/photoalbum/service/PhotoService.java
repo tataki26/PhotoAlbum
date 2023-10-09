d19cc08d384f57d72751267349f0637b8f0b047f
tataki26 <tataki26@gmail.com>
2023-10-09 17:38:33 +0900

    [service] 사진 업로드 API Service 코드 구현 1
    1. id로 앨범 존재 여부 확인
    2. 사진 파일명, 용량 정의

--- server/photoalbum/src/main/java/com/tataki26/photoalbum/service/PhotoService.java
+++ server/photoalbum/src/main/java/com/tataki26/photoalbum/service/PhotoService.java
@@ -1,16 +1,24 @@
 package com.tataki26.photoalbum.service;
 
+import com.tataki26.photoalbum.Constants;
 import com.tataki26.photoalbum.domain.Album;
 import com.tataki26.photoalbum.domain.Photo;
-import com.tataki26.photoalbum.dto.AlbumDto;
 import com.tataki26.photoalbum.dto.PhotoDto;
 import com.tataki26.photoalbum.mapper.PhotoMapper;
 import com.tataki26.photoalbum.repository.AlbumRepository;
 import com.tataki26.photoalbum.repository.PhotoRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,6 +45,18 @@ public class PhotoService {
                 return photoDto;
             }
         }
+
         throw new EntityNotFoundException(String.format("ID %d로 조회된 사진이 없습니다", photoId));
+    }
+
+    public PhotoDto savePhoto(Long albumId, MultipartFile file) {
+        // get album
+        Optional<Album> albumOptional = albumRepository.findById(albumId);
+        if (albumOptional.isEmpty()) {
+            throw new EntityNotFoundException(String.format("ID %d로 조회된 앨범이 없습니다", albumId));
+        }
+
+        String originalFileName = file.getOriginalFilename();
+        int fileSize = (int)file.getSize();
     }
 }
