780cb53fb848de13b3bd64e2d832732910db65dc
tataki26 <tataki26@gmail.com>
2023-10-18 21:51:23 +0900

    [test] UserServiceTest 버그 해결
    - 테스트 데이터가 db에 반영되는 문제 해결
    - @Transactional 추가

--- server/photoalbum/src/test/java/com/tataki26/photoalbum/service/UserServiceTest.java
+++ server/photoalbum/src/test/java/com/tataki26/photoalbum/service/UserServiceTest.java
@@ -4,10 +4,12 @@ import com.tataki26.photoalbum.dto.UserDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class UserServiceTest {
     @Autowired
     private UserService userService;
