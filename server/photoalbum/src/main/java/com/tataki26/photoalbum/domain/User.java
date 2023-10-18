4351d9da15561960013a920c16c2c4e1a4fdff5d
tataki26 <tataki26@gmail.com>
2023-10-18 16:09:40 +0900

    [domain] UserDto 값으로 User 객체 생성을 위한 메서드 추가
    - db에 저장할 User 객체 생성을 위해 User 엔티티에 createUser 메서드 추가
    - dto의 필드 값을 검증하고 User 객체 생성 후 반환

--- server/photoalbum/src/main/java/com/tataki26/photoalbum/domain/User.java
+++ server/photoalbum/src/main/java/com/tataki26/photoalbum/domain/User.java
@@ -1,5 +1,6 @@
 package com.tataki26.photoalbum.domain;
 
+import com.tataki26.photoalbum.dto.UserDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,5 +28,31 @@ public class User {
 
     public void setLoginAt(Date loginAt) {
         this.loginAt = loginAt;
+    }
+
+    public static User createUser(UserDto userDto) {
+        String name = userDto.getName();
+        String email = userDto.getEmail();
+        String password = userDto.getPassword();
+
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("회원 이름은 생략할 수 없습니다.");
+        }
+
+        if (email == null || email.isEmpty()) {
+            throw new IllegalArgumentException("회원 이메일은 생략할 수 없습니다.");
+        }
+
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("비밀 번호는 생략할 수 없습니다.");
+        }
+
+        User user = new User();
+
+        user.name = name;
+        user.email = email;
+        user.password = password;
+
+        return user;
     }
 }
