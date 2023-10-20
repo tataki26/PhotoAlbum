981fd2cf94bb3d9da55577817d0ef773d6d10a81
tataki26 <tataki26@gmail.com>
2023-10-21 00:37:41 +0900

    [security] Spring Security 기본 설정
    - ** 기존 코드 초기화(JWT 관련 코드 제거) **
    
    - Spring Security 필터 체인 정의
    - 모든 요청에 인증 필요(인증 없이 접근할 경우 403 에러)

--- server/photoalbum/src/main/java/com/tataki26/photoalbum/security/JwtSecurityConfiguration.java
+++ server/photoalbum/src/main/java/com/tataki26/photoalbum/security/JwtSecurityConfiguration.java
@@ -1,36 +1,12 @@
 package com.tataki26.photoalbum.security;
 
-import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
-
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.interfaces.RSAPublicKey;
-import java.util.UUID;
-
-import static org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION;
 
 @Configuration
 public class JwtSecurityConfiguration {
@@ -45,90 +21,6 @@ public class JwtSecurityConfiguration {
                 .httpBasic(HttpBasicConfigurer::disable)
                 // csrf disable
                 .csrf(CsrfConfigurer::disable)
-                // same origin request -> enable frame option (h2-console)
-                .headers(customizer -> customizer.frameOptions(frame -> frame.sameOrigin()))
-                // enable jwt authentication within oauth2 resource server
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                .build();
-    }
-
-    // set DataSource(to manage database connection)
-    @Bean
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                // script for ddl
-                .addScript(DEFAULT_USER_SCHEMA_DDL_LOCATION)
-                .build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        UserDetails user = User.withUsername("user")
-                .password("user1234")
-                .passwordEncoder(str -> passwordEncoder().encode(str))
-                .roles(UserRoles.USER.name())
-                .build();
-
-        UserDetails admin = User.withUsername("admin")
-                .password("admin1234")
-                .passwordEncoder(str -> passwordEncoder().encode(str))
-                .roles(UserRoles.ADMIN.name())
-                .build();
-
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-        // jdbcUserDetailsManager.createUser(user);
-        // jdbcUserDetailsManager.createUser(admin);
-
-        return jdbcUserDetailsManager;
-    }
-
-    private BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    // create key pair (2048 bits) for RSAKey
-    // key pair: public key & private key
-    @Bean
-    public KeyPair keyPair() {
-        try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-            keyPairGenerator.initialize(2048);
-            return keyPairGenerator.generateKeyPair();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    // create RSA key
-    @Bean
-    public RSAKey rsaKey(KeyPair keyPair) {
-        return new RSAKey
-                // set public key - verify jwt
-                .Builder((RSAPublicKey) keyPair.getPublic())
-                // set private key - create jwt (header, payload)
-                .privateKey(keyPair.getPrivate())
-                // set key id (unique id for RSAKey)
-                .keyID(UUID.randomUUID().toString())
-                .build();
-    }
-
-    // JWK(JSON Web Key)
-    @Bean
-    // JWKSource provides the information of jwt public key
-    public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
-        // JWKSet: the set of JWK
-        JWKSet jwkSet = new JWKSet(rsaKey);
-        // Return a JWKSource implementation using a lambda expression
-        return ((jwkSelector, context) -> jwkSelector.select(jwkSet));
-    }
-
-    // create JwtDecoder
-    // use public key to verify signature
-    @Bean
-    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
-        return NimbusJwtDecoder
-                .withPublicKey(rsaKey.toRSAPublicKey())
                 .build();
     }
 }
