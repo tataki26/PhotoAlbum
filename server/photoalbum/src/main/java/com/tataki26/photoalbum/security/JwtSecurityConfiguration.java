90800eb0a838594529dbf0f2bf3d04cfafe34bfa
tataki26 <tataki26@gmail.com>
2023-10-21 00:51:07 +0900

    [security] 회원 가입 API 허용 url 설정
    - 권한이 없는 상태에서도 회원 가입이 가능하도록 허용 url로 설정
    - h2 console도 권한 없이 접근 허용 설정
    - 그 외 요청은 인증 필요

--- server/photoalbum/src/main/java/com/tataki26/photoalbum/security/JwtSecurityConfiguration.java
+++ server/photoalbum/src/main/java/com/tataki26/photoalbum/security/JwtSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.tataki26.photoalbum.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,11 +11,17 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class JwtSecurityConfiguration {
+    private final String allowedUrl = "/users";
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 // apply authentication to all requests
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers(allowedUrl).permitAll()
+                               .requestMatchers(PathRequest.toH2Console()).permitAll()
+                               .anyRequest().authenticated()
+                )
                 // set session stateless
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // disable http basic authentication (use jwt instead)
