0308c1b29d9d3a1ca74a28d4df5d08308c3e2e13
tataki26 <tataki26@gmail.com>
2023-10-20 10:06:39 +0900

    [security] DataSource 및 UserDetailsService 추가
    - 회원 정보를 h2 내장 데이터베이스에서 관리하기 위해 DataSource 추가(+ 스키마 설정 스크립트 추가)
    
    - 사용자 인증 및 권한 부여에 사용되는 UserDetailsService 인터페이스 Bean 등록
    - 사용자 정보를 나타내는 UserDetails 인터페이스 구현(User 빌더 메서드 사용)
    - BCryptPasswordEncoder: BCrypt 해시 알고리즘을 사용하여 비밀번호를 암호화하는 객체
    - 데이터베이스에서 사용자 정보를 관리하는  JdbcUserDetailsManager 객체를 생성하고 createUser로 사용자 추가
    
    - 사용자 권한을 위한 Enum 클래스 정의

--- server/photoalbum/src/main/java/com/tataki26/photoalbum/security/JwtSecurityConfiguration.java
+++ server/photoalbum/src/main/java/com/tataki26/photoalbum/security/JwtSecurityConfiguration.java
@@ -2,12 +2,23 @@ package com.tataki26.photoalbum.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION;
 
 @Configuration
 public class JwtSecurityConfiguration {
@@ -27,5 +38,40 @@ public class JwtSecurityConfiguration {
                 // enable jwt authentication within oauth2 resource server
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
+    }
+
+    // set DataSource(to manage database connection)
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                // script for ddl
+                .addScript(DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        UserDetails user = User.withUsername("user")
+                .password("user1234")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles(UserRoles.USER.name())
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("admin1234")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles(UserRoles.ADMIN.name())
+                .build();
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+
+        return jdbcUserDetailsManager;
+    }
+
+    private BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ server/photoalbum/src/main/java/com/tataki26/photoalbum/security/UserRoles.java
@@ -0,0 +1,5 @@
+package com.tataki26.photoalbum.security;
+
+public enum UserRoles {
+    USER, ADMIN
+}
