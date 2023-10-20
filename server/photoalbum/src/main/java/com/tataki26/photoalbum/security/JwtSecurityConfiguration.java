62fd8f572ad3ae41577f796cc2bd8f902e8d3e8e
tataki26 <tataki26@gmail.com>
2023-10-20 17:30:24 +0900

    [security] RSA 암호화 키 쌍 및 JWT Decoder 생성
    - JWT 디코더 설정을 위한 RSA 암호화 키 쌍 생성
    
    - jwt 수신 측에서 문자열로 인코딩된 JWT를 디코딩하여 헤더, 클레임, 서명 등의 구성 요소로 분석
    
    - key pair >> rsa key >> jwk source  / jwt decoder
    
    - ** JWT는 데이터 무결성을 확인하고 클라이언트와 서버 간 사용자 인증 및 권한 정보를 전달하기 위해 사용 **
    
    - ** USER 엔티티 충돌 문제로 인해 create user 주석 처리 **

--- server/photoalbum/src/main/java/com/tataki26/photoalbum/security/JwtSecurityConfiguration.java
+++ server/photoalbum/src/main/java/com/tataki26/photoalbum/security/JwtSecurityConfiguration.java
@@ -1,5 +1,10 @@
 package com.tataki26.photoalbum.security;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -13,10 +18,17 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 
 import static org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION;
 
@@ -65,13 +77,58 @@ public class JwtSecurityConfiguration {
                 .build();
 
         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-        jdbcUserDetailsManager.createUser(user);
-        jdbcUserDetailsManager.createUser(admin);
+        // jdbcUserDetailsManager.createUser(user);
+        // jdbcUserDetailsManager.createUser(admin);
 
         return jdbcUserDetailsManager;
     }
 
     private BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // create key pair (2048 bits) for RSAKey
+    // key pair: public key & private key
+    @Bean
+    public KeyPair keyPair() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // create RSA key
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair) {
+        return new RSAKey
+                // set public key - verify jwt
+                .Builder((RSAPublicKey) keyPair.getPublic())
+                // set private key - create jwt (header, payload)
+                .privateKey(keyPair.getPrivate())
+                // set key id (unique id for RSAKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    // JWK(JSON Web Key)
+    @Bean
+    // JWKSource provides the information of jwt public key
+    public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+        // JWKSet: the set of JWK
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        // Return a JWKSource implementation using a lambda expression
+        return ((jwkSelector, context) -> jwkSelector.select(jwkSet));
+    }
+
+    // create JwtDecoder
+    // use public key to verify signature
+    @Bean
+    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+        return NimbusJwtDecoder
+                .withPublicKey(rsaKey.toRSAPublicKey())
+                .build();
     }
 }
