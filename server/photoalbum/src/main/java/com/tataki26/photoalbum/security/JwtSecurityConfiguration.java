78a0b56fa618504491936a5d8074a95f05a1cd03
tataki26 <tataki26@gmail.com>
2023-10-21 15:33:53 +0900

    [security] RSA 암호화 키 쌍 및 JWT Decoder / Encoder 생성
    - JWT 디코더 설정을 위한 RSA 암호화 키 쌍 생성
    - JWT 디코더 및 인코더 추가
    
    - key pair >> rsa key >> jwk source / jwt decoder
    
    - ** JWT는 데이터 무결성을 확인하고 클라이언트와 서버 간 사용자 인증 및 권한 정보를 전달하기 위해 사용 **
    - JWT 수신 측에서 생성 시점에 문자열로 인코딩된 JWT를 디코딩하여 헤더, 클레임, 서명 등의 구성 요소로 분석

--- server/photoalbum/src/main/java/com/tataki26/photoalbum/security/JwtSecurityConfiguration.java
+++ server/photoalbum/src/main/java/com/tataki26/photoalbum/security/JwtSecurityConfiguration.java
@@ -1,15 +1,30 @@
 package com.tataki26.photoalbum.security;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 
 @Configuration
 public class JwtSecurityConfiguration {
@@ -30,11 +45,62 @@ public class JwtSecurityConfiguration {
                 .httpBasic(HttpBasicConfigurer::disable)
                 // csrf disable
                 .csrf(CsrfConfigurer::disable)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // create key pair (2048 bits) for RSAKey
+    // key pair: public key & private key
+    @Bean
+    public KeyPair keyPair() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // create RSA key
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair) {
+        return new RSAKey
+                // set public key - verify jwt
+                .Builder((RSAPublicKey) keyPair.getPublic())
+                // set private key - create jwt (header, payload)
+                .privateKey(keyPair.getPrivate())
+                // set key id (unique id for RSAKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    // JWK(JSON Web Key)
+    @Bean
+    // JWKSource provides the information of jwt public key
+    public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+        // JWKSet: the set of JWK
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        // Return a JWKSource implementation using a lambda expression
+        return ((jwkSelector, context) -> jwkSelector.select(jwkSet));
+    }
+
+    // create JwtDecoder
+    // use public key to verify signature
+    @Bean
+    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+        return NimbusJwtDecoder
+                .withPublicKey(rsaKey.toRSAPublicKey())
+                .build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) {
+        return new NimbusJwtEncoder(jwkSource);
     }
 }
