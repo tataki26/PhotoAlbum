4709f358f68e51741772b3e1da33bbaa42f2abdb
tataki26 <tataki26@gmail.com>
2023-11-04 17:31:16 +0900

    [login] 로그인 컴포넌트에 에러 메시지 표시
    - javascript 표현식 적용
    - 로그인 버튼 이벤트 핸들러 정의
    - 아이디나 비밀번호가 일치하지 않으면 에러 메시지 출력

--- client/src/components/PhotoAlbum.jsx
+++ client/src/components/PhotoAlbum.jsx
@@ -13,6 +13,8 @@ function LoginComponent() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
+
     function handleEmailChange(e) {
         setEmail(e.target.value);
     }
@@ -21,8 +23,18 @@ function LoginComponent() {
         setPassword(e.target.value);
     }
 
+    function handleSubmit() {
+        if (email !== "abc@test.com" || password !== "1234") {
+            setShowErrorMessage(true);
+        }
+        else {
+            setShowErrorMessage(false);
+        }
+    }
+
     return (
         <div className="Login">
+            {showErrorMessage && <div className="errorMessage">아이디 또는 비밀번호가 일치하지 않습니다</div>}
             <div className="LoginForm">
                 <div>
                     <input type="text" placeholder="이메일 입력" name="email" value={email} onChange={handleEmailChange}/>
@@ -31,7 +43,7 @@ function LoginComponent() {
                     <input type="password" placeholder="비밀번호 입력" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
-                    <button type="button" name="login">로그인</button>
+                    <button type="button" name="login" onClick={handleSubmit}>로그인</button>
                 </div>
             </div>
         </div>
