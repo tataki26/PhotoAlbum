60814aa508288a44d314a2442956f425ac0780b8
tataki26 <tataki26@gmail.com>
2023-11-04 16:10:13 +0900

    [login] 로그인 컴포넌트 state 추가
    - input에 value 설정하고 이벤트 핸들러 등록
    - email, password state 정의
    - input에 입력되는 값을 DOM이 아닌 React가 관리할 수 있다(제어 컴포넌트)
    
    - css 추가

--- client/src/components/PhotoAlbum.jsx
+++ client/src/components/PhotoAlbum.jsx
@@ -1,21 +1,34 @@
+import { useState } from 'react'
+import './PhotoAlbum.css'
+
 export default function PhotoAlbum() {
     return (
         <div className="PhotoAlbum">
-            Photo Album Application
             <LoginComponent />
         </div>
     )
 }
 
 function LoginComponent() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleEmailChange(e) {
+        setEmail(e.target.value);
+    }
+
+    function handlePasswordChange(e) {
+        setPassword(e.target.value);
+    }
+
     return (
         <div className="Login">
             <div className="LoginForm">
                 <div>
-                    <input type="text" placeholder="이메일 입력" name="email" />
+                    <input type="text" placeholder="이메일 입력" name="email" value={email} onChange={handleEmailChange}/>
                 </div>
                 <div>
-                    <input type="password" placeholder="비밀번호 입력" name="password" />
+                    <input type="password" placeholder="비밀번호 입력" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
                     <button type="button" name="login">로그인</button>
