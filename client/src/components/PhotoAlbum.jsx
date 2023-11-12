96f9b2c88e9ef6bf499433f6a00b782134152348
tataki26 <tataki26@gmail.com>
2023-11-12 21:11:11 +0900

    [auth] 인증 라우터 구현
    - 로그인 시에만 특정 페이지에 접근이 가능하도록 인증 라우터 구현
    - 로그인 상태이면 페이지를 반환하고 그렇지 않으면 로그인 페이지로 리다이렉트

--- client/src/components/PhotoAlbum.jsx
+++ client/src/components/PhotoAlbum.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuth } from './security/AuthContext';
 import RootComponent from './RootComponent';
 import LoginComponent from './LoginComponent';
 import LogoutComponent from './LogoutComponent';
@@ -10,6 +11,15 @@ import ErrorComponent from './ErrorComponent';
 import AuthProvider from './security/AuthContext';
 import './PhotoAlbum.css'
 
+function AuthenticatedRoute({ children }) {
+    const authContext = useAuth();
+    
+    if (authContext.isAuthenticated)
+        return children;
+
+    return <Navigate to="/login" />
+}
+
 export default function PhotoAlbum() {
     return (
         <div className="PhotoAlbum">
@@ -19,10 +29,22 @@ export default function PhotoAlbum() {
                     <Routes>
                         <Route path="/" element={<RootComponent />} />
                         <Route path="/login" element={<LoginComponent />} />
-                        <Route path="/logout" element={<LogoutComponent />} />
+                        <Route path="/logout" element={
+                            <AuthenticatedRoute>
+                                <LogoutComponent />
+                            </AuthenticatedRoute>
+                        } />
                         <Route path="/users" element={<UserComponent />} />
-                        <Route path="/welcome/:username" element={<WelcomeComponent />} />
-                        <Route path="/albums" element={<ListAlbumComponent />} />
+                        <Route path="/welcome/:username" element={
+                            <AuthenticatedRoute>
+                                <WelcomeComponent />
+                            </AuthenticatedRoute>
+                        } />
+                        <Route path="/albums" element={
+                            <AuthenticatedRoute>
+                                <ListAlbumComponent />
+                            </AuthenticatedRoute>
+                        } />
                         <Route path="*" element={<ErrorComponent />} />
                     </Routes>
                 </BrowserRouter>
