f14d261759dedac75196f222949c903555e2ef7f
tataki26 <tataki26@gmail.com>
2023-11-13 11:12:53 +0900

    [api] axios 설치 및 백엔드 CORS 요청 활성화
    - 프론트엔드에서 백엔드 api를 호출하기 위한 axios 라이브러리 설치
    
    - CORS 요청을 허용하기 위한 설정 추가
    - 미리 정의된 WebMvcConfigurer의 메서드를 오버라이드

--- server/photoalbum/src/main/java/com/tataki26/photoalbum/PhotoalbumApplication.java
+++ server/photoalbum/src/main/java/com/tataki26/photoalbum/PhotoalbumApplication.java
@@ -2,6 +2,9 @@ package com.tataki26.photoalbum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PhotoalbumApplication {
@@ -10,4 +13,15 @@ public class PhotoalbumApplication {
 		SpringApplication.run(PhotoalbumApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
