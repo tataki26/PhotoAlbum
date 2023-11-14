3949820bf7e13c0ef30832b6527086bc14507495
tataki26 <tataki26@gmail.com>
2023-11-14 23:03:53 +0900

    [client] 헤더 컴포넌트 스타일 업데이트
    - 링크 추가하고 로그 이미지 적용
    - 기존 navbar-brand에 스타일 적용
    - main 링크 제외한 나머지 링크 margin 변경
    
    - ** 부트스트랩 스타일 변경하기 위해 우선 순위가 높은 id와 인라인 스타일 적용 **

--- client/src/components/HeaderComponent.jsx
+++ client/src/components/HeaderComponent.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useAuth } from './security/AuthContext';
+import logo from '../assets/pngs/ph_diamond.png';
 
 export default function HeaderComponent() {
     const authContext = useAuth();
@@ -10,29 +11,32 @@ export default function HeaderComponent() {
             <div className="container">
                 <div className="row">
                     <nav className="navbar navbar-expand-lg">
-                        <Link className="navbar-brand ms-2 fs-2 fw-bold text-black" to="http://localhost:3000">PhotoAlbum</Link>
+                        <Link to="http://localhost:3000" className="LogoImg2">
+                            <img src={logo} alt="logo" />
+                        </Link>
+                        <Link id="customNavbarLink" className="navbar-brand ms-2 fs-2" style={{color: "#A44A21"}} to="http://localhost:3000">PhotoAlbum</Link>
                         <div className="collapse navbar-collapse">
                             <ul className="navbar-nav">
-                                <li className="nav-item fs-5">
+                                <li className="nav-item fs-5 mt-2">
                                     {isAuthenticated
                                         && <Link className="nav-link" to="/welcome/abc">Home</Link>}
                                 </li>
-                                <li className="nav-item fs-5">
+                                <li className="nav-item fs-5 mt-2">
                                     {isAuthenticated
                                         && <Link className="nav-link" to="/albums">Albums</Link>}
                                 </li>
                             </ul>
                         </div>
                         <ul className="navbar-nav">
-                            <li className="nav-item fs-5">
+                            <li className="nav-item fs-5 mt-2">
                                 {!isAuthenticated
                                     &&<Link className="nav-link" to="/login">Login</Link>}
                             </li>
-                            <li className="nav-item fs-5">
+                            <li className="nav-item fs-5 mt-2">
                                 {isAuthenticated
                                     && <Link className="nav-link" to="/logout" onClick={() => authContext.logout()}>Logout</Link>}
                             </li>
-                            <li className="nav-item fs-5">
+                            <li className="nav-item fs-5 mt-2">
                                 {!isAuthenticated
                                     &&<Link className="nav-link" to="/users">Join Us</Link>}
                             </li>
