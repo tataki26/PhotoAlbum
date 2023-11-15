6fde7e68485b276e3f05426f9eb5c5dfc0be2c29
tataki26 <tataki26@gmail.com>
2023-11-15 23:41:12 +0900

    [client] 앨범 목록 페이지 UI 추가
    - 앨범 목록 페이지 상단 바 UI 추가

--- client/src/components/ListAlbumComponent.jsx
+++ client/src/components/ListAlbumComponent.jsx
@@ -1,10 +1,40 @@
+import vector5 from '../assets/svgs/vector5.svg';
+import downArrow from '../assets/svgs/down-arrow.svg';
+
 export default function ListAlbumComponent() {
     return (
-        <div className="ListAlbumComponent">
-            <h1>내 사진첩</h1>
-            <div>
-                Album Details
-            </div>
+        <div>
+            <header className="border-bottom border-light border-5 mb-5 p-2" style={{"margin-top": "-70px"}}>
+                <div className="container">
+                    <div className="row">
+                        <nav className="navbar navbar-expand-lg">
+                            <div className="collapse navbar-collapse">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item fs-5 mt-5">
+                                    <label className="AlbumLabel">Galleries</label>
+                                    </li>
+                                </ul>
+                            </div>
+                            <ul className="navbar-nav">
+                                <li className="nav-item fs-5 mt-5">
+                                    <label className="AlbumSortLabel">생성 날짜 순</label>
+                                </li>
+                                <li className="nav-item fs-5 mt-5">
+                                    <button className="AlbumSortButton">
+                                        <img src={downArrow} alt="down arrow" />
+                                    </button>
+                                </li>
+                                <li className="nav-item fs-5 mt-5">
+                                    <img src={vector5} alt="vector 5" style={{"marginLeft": "20px"}}/>
+                                </li>
+                                <li className="nav-item fs-5 mt-5">
+                                    <input className="AlbumSearchInput" placeholder="갤러리 검색" type="text" />
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
         </div>
-    )
+    )   
 }
