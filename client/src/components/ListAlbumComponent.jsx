090eeee508b1d8eb4937d24a365ba227faab7d47
tataki26 <tataki26@gmail.com>
2023-11-18 23:49:02 +0900

    [client] 앨범 목록 페이지 UI 추가 2
    - 테이블 추가
    - 정적 앨범 리스트 추가

--- client/src/components/ListAlbumComponent.jsx
+++ client/src/components/ListAlbumComponent.jsx
@@ -1,7 +1,25 @@
+import { useNavigate } from 'react-router-dom';
 import vector5 from '../assets/svgs/vector5.svg';
 import downArrow from '../assets/svgs/down-arrow.svg';
+import User01c from '../assets/pngs/User01c.png';
+import User02c from '../assets/pngs/User02c.png';
+import User03c from '../assets/pngs/User03c.png';
+import User04c from '../assets/pngs/User04c.png';
 
 export default function ListAlbumComponent() {
+    const albumList = [
+        {id: 1, name: "2023_01", count: 4, photo: User01c},
+        {id: 2, name: "2023_02", count: 10, photo: User02c},
+        {id: 3, name: "2023_03", count: 25, photo: User03c},
+        {id: 4, name: "2023_04", count: 1, photo: User04c}
+    ]
+    
+    const navigate = useNavigate();
+               
+    function handleClick() {
+        navigate("/");
+    }
+
     return (
         <div>
             <header className="border-bottom border-light border-5 mb-5 p-2" style={{"margin-top": "-70px"}}>
@@ -35,6 +53,32 @@ export default function ListAlbumComponent() {
                     </div>
                 </div>
             </header>
+            <div>
+                <div style={{maxWidth: "1300px", overflowX: "auto", marginLeft: "200px"}}>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <td>앨범 이름</td>
+                                <td>사진 수</td>
+                                <td>썸네일</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            albumList.map(
+                                albumList => (
+                                    <tr key={albumList.id}>
+                                        <td>{albumList.name}</td>
+                                        <td>{albumList.count}</td>
+                                        <td onClick={handleClick} style={{ cursor: 'pointer' }}><img src={albumList.photo} alt="user photo" /></td>
+                                    </tr>
+                                )
+                            )
+                        }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     )   
 }
