e5522a7570f8341163d4dd385cf83611c24f7088
tataki26 <tataki26@gmail.com>
2023-11-23 23:45:29 +0900

    [album] 앨범 세부 정보 페이지 업데이트
    - 사진 목록을 출력하는 테이블 추가
    - 하드코딩된 이미지 사용
    - ** 추후 api의 응답 데이터로 받은 이미지를 사용하도록 로직 변경 필요**

--- client/src/components/AlbumComponent.jsx
+++ client/src/components/AlbumComponent.jsx
@@ -1,5 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from 'react';
+import User01c from '../assets/pngs/User01c.png';
+import User02c from '../assets/pngs/User02c.png';
+import User03c from '../assets/pngs/User03c.png';
+import User04c from '../assets/pngs/User04c.png';
 import { retrieveAlbumApi } from '../apis/AlbumApiService';
 
 export default function AlbumComponent() {
@@ -20,7 +24,34 @@ export default function AlbumComponent() {
         [id]
     );
 
+    const imageArray = [User01c, User02c, User03c, User04c, User01c];
+
     return (
-        <h1>{name}</h1>
+        <div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Photos</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr key={id}>
+                        <td>{id}</td>
+                        <td>
+                            <div className="container">
+                                <div className="row">
+                                    {imageArray.map((image, index) => (
+                                        <div className="col" key={index}>
+                                            <img src={image} alt={`user photo ${index + 1}`} className="img-thumbnail" />
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
